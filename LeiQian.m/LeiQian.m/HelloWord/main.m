6829246c5c474e652232c17533142ee9a55253bc
qizhemotuosangeyan <44314214+qizhemotuosangeyan@users.noreply.github.com>
2019-05-16 21:22:45 +0800

    雷千
    Work 1

---
+++ LeiQian.m/LeiQian.m/HelloWord/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HelloWord
+//
+//  Created by 千千 on 2019/5/14.
+//  Copyright © 2019 千千. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
