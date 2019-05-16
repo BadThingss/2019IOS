e22c71887d984bb4e30da6cbc1a7c20cf25477d9
qizhemotuosangeyan <44314214+qizhemotuosangeyan@users.noreply.github.com>
2019-05-16 21:28:53 +0800

    雷千Work1
    work1

---
+++ LeiQian.m/HelloWord/ViewController.m
@@ -0,0 +1,24 @@
+//
+//  ViewController.m
+//  HelloWord
+//
+//  Created by 千千 on 2019/5/14.
+//  Copyright © 2019 千千. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"Hello world");
+    // Do any additional setup after loading the view.
+}
+
+
+@end
