82d43b8f0a832c149f18bcc2156a2db2bca4bbbd
mtcoafun <50589627+mtcoafun@users.noreply.github.com>
2019-05-19 23:11:33 +0800

    陈颀玮work1
    陈颀玮work1

---
+++ 陈颀玮helol objective-c/hell objective-c/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  hell objective-c
+//
+//  Created by 陈颀玮 on 2019/5/19.
+//  Copyright © 2019 陈颀玮. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");//@是oc语言特有的字符串类型，可以自动换行
+        printf("hello,world\n");//是c语言的打印方式
+    }
+    return 0;
+}
