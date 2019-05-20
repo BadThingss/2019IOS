64ba6281c87a01fa58ee8c2b1b38316ae808aa10
Your Name <1216724577@qq.com>
2019-05-20 23:55:33 +0800

    yyy

---
+++ main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Work1
+//
+//  Created by mac on 2019/5/20.
+//  Copyright © 2019 mac. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
