200ea3cb51219afbbbb2713bfc2bb8844ff07fc6
karen <951183596@qq.com>
2019-06-02 22:46:29 +0800

    第二次修改

--- Work3_周凯伦/可爱程序/ViewController.m
+++ Work3_周凯伦/可爱程序/ViewController.m
@@ -1,6 +1,15 @@
+//
+//  ViewController.m
+//  可爱程序
+//
+//  Created by karen on 2019/6/2.
+//  Copyright © 2019 Karen. All rights reserved.
+//
+
 #import "ViewController.h"
 
 @interface ViewController ()<UITableViewDataSource>
+
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @end
@@ -37,27 +46,22 @@
         if (indexPath.row == 0){
             cell.textLabel.text = @"企鹅🐧";
             cell.imageView.image = [UIImage imageNamed:@"企鹅"];
-            cell.accessoryType = UITableViewCellAccessoryCheckmark;
-//(打勾） cell.accessoryType = UITableViewCellAccessoryCheckmark;
-// (开关）cell.accessoryType = [[UISwitch alloc] init];
+            //(打勾） cell.accessoryType = UITableViewCellAccessoryCheckmark;
+            // (开关）cell.accessoryType = [[UISwitch alloc] init];
         }else if (indexPath.row == 1){
             cell.textLabel.text = @"熊熊🐻";
             cell.imageView.image = [UIImage imageNamed:@"熊熊"];
-            cell.accessoryType = UITableViewCellAccessoryCheckmark;
         }
     }else if (indexPath.section == 1){
         if (indexPath.row == 0){
             cell.textLabel.text = @"熊猫🐼";
             cell.imageView.image = [UIImage imageNamed:@"熊猫"];
-            cell.accessoryType = UITableViewCellAccessoryCheckmark;
         }else if (indexPath.row == 1){
             cell.textLabel.text = @"蝴蝶🦋";
             cell.imageView.image = [UIImage imageNamed:@"蝴蝶"];
-            cell.accessoryView = [[UISwitch alloc] init];
         }else if (indexPath.row == 2){
             cell.textLabel.text = @"狐狸🦊";
             cell.imageView.image = [UIImage imageNamed:@"狐狸"];
-            cell.accessoryType = UITableViewCellAccessoryCheckmark;
         }
     }
     return cell;
@@ -71,6 +75,13 @@
         return @"巨可爱的嘞";
     }
 }
+
+
+
+
+
+
+
 
 
 @end
--- Work3_周凯伦/可爱程序/main.m
+++ Work3_周凯伦/可爱程序/main.m
@@ -1,6 +1,6 @@
 //
 //  main.m
-//  Work3_周凯伦
+//  可爱程序
 //
 //  Created by karen on 2019/6/2.
 //  Copyright © 2019 Karen. All rights reserved.
